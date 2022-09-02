58bbeff862365768e82d5dcbf08a29dd06d69b65
iceqi <qqwei1123@163.com>
2022-09-02 15:53:36 +0800

    init

--- src/player.cpp
+++ src/player.cpp
@@ -5,8 +5,7 @@ using namespace godot;
 void Player::_register_methods() {
   register_method("_process", &Player::_process);
   register_property<Player, float>("amplitude", &Player::amplitude, 10.0);
-  register_property<Player, float>("speed", &Player::set_speed,
-                                   &Player::get_speed, 1.0);
+  register_property<Player, float>("speed", &Player::set_speed, &Player::get_speed, 1.0);
 }
 
 Player::Player() {}
@@ -25,15 +24,17 @@ void Player::_init() {
 void Player::_process(float delta) {
   time_passed += speed * delta;
 
-  Vector2 new_position =
-      Vector2(amplitude + (amplitude * sin(time_passed * 2.0)),
-              amplitude + (amplitude * cos(time_passed * 1.5)));
+	Vector2 new_position = Vector2(
+		amplitude + (amplitude * sin(time_passed * 2.0)),
+		amplitude + (amplitude * cos(time_passed * 1.5))
+	);
+
+  set_position(new_position);
   time_emit += delta;
   if (time_emit > 1.0) {
     time_emit = 0.0;
   }
 
-  set_position(new_position);
 }
 
 void Player::set_speed(float p_speed) { speed = p_speed; }
